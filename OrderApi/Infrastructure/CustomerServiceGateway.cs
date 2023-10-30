26df6a55ca1e170f4017f117f581a273ed7b2eba
saidr <saidradwanmansour@hotmail.com>
2023-10-30 12:46:34 +0100

    Retry pattern implemented

--- OrderApi/Infrastructure/CustomerServiceGateway.cs
+++ OrderApi/Infrastructure/CustomerServiceGateway.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using RestSharp;
 using SharedModels;
+using Polly;
+using Polly.Retry;
 
 namespace OrderApi.Infrastructure
 {
     public class CustomerServiceGateway : IServiceGateway<CustomerDto>
     {
         string customerServiceBaseUrl;
+        private readonly AsyncRetryPolicy _retryPolicy;
 
         public CustomerServiceGateway(string baseUrl)
         {
             customerServiceBaseUrl = baseUrl;
+
+            _retryPolicy = Policy
+            .Handle<HttpRequestException>()
+            .WaitAndRetryAsync(10, retryAttempt =>
+                TimeSpan.FromSeconds(3), onRetry: (exception, timeSpan, retryCount, context) =>
+                {
+                    Console.WriteLine($"Retry {retryCount}. Attemp failed, trying again...");
+                }
+            );
         }
 
         public async Task<CustomerDto> GetAsync(int id)
@@ -18,7 +30,13 @@ namespace OrderApi.Infrastructure
             RestClient c = new RestClient(customerServiceBaseUrl);
 
             var request = new RestRequest(id.ToString());
-            return await c.GetAsync<CustomerDto>(request);
+            var resp = await _retryPolicy.ExecuteAsync(async () =>
+            {
+                var response = await c.GetAsync<CustomerDto>(request);
+                return response;
+
+            });
+            return resp;       
         }
     }
 }
